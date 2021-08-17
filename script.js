032e02fca98d4c29fcbcd0c725822d45f449a14f
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-08-17 08:41:22 -0300

    Adicionando detalhes

--- script.js
+++ script.js
@@ -10,26 +10,27 @@ const calcular = document.getElementById("calcular");
 
 function calculando() {
     if(peso.value !== "" && altura.value !== ""){
-        imc = parseInt(peso.value) / parseInt(altura.value*2); 
+        imc = parseFloat(peso.value.replace(',', '.')) / parseFloat(altura.value.replace(',', '.')*2); 
+    
         //conversão de string para número
         //mesmo que coloquemos o input como number, devemos colocar o parse para a conversão de String para numero    
         if(imc < 18.5){
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está abaixo do peso ideal";
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está abaixo do peso ideal";
         }
         else if(imc >= 18.5 && imc <= 29.9 ){
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está com o peso ideal. Parabéns!!!";
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está com o peso ideal. Parabéns!!!";
         }
         else if(imc >= 25 && imc <= 29.9 ){
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está acima do peso ideal";
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está acima do peso ideal";
         }
         else if(imc >= 30 && imc <= 34.9){
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está com obesidade grau I";
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está com obesidade grau I";
         }
         else if(resultado >= 35 && imc <= 39.9){
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está com obesidade grau II"; 
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está com obesidade grau II"; 
         }
         else{
-            resultado.innerHTML = nome.value + " seu IMC é " + imc + " e você está com obesidade gru III";
+            resultado.innerHTML = nome.value + " seu IMC é " + imc.toFixed(2) + " e você está com obesidade grau III";
         }
     }
     else{
